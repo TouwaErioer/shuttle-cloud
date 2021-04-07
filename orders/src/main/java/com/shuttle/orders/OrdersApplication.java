0682f6a422f3e4312800a8ecc261febd0cc6744e
TouwaErioer <duhongyu627@gmail.com>
2021-04-07 23:59:55 +0800

    fix(orders):inject ServerEndpointExporter

--- orders/src/main/java/com/shuttle/orders/OrdersApplication.java
+++ orders/src/main/java/com/shuttle/orders/OrdersApplication.java
@@ -3,9 +3,10 @@ package com.shuttle.orders;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 @MapperScan("com.shuttle.orders.mapper")
 @EnableFeignClients
@@ -17,4 +18,11 @@ public class OrdersApplication {
         SpringApplication.run(OrdersApplication.class, args);
     }
 
+    /**
+     * 注入一个ServerEndpointExporter,该Bean会自动注册使用@ServerEndpoint注解申明的websocket endpoint
+     */
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
 }
